7a62c42ce9b55fe040bf0975eabecb6924731c7a
Baderaum <32603145+Baderaum@users.noreply.github.com>
2026-05-23 20:08:44 +0200

    Fix Logout (#7)

--- app/auth/signout/route.ts
+++ app/auth/signout/route.ts
@@ -1,18 +1,12 @@
 import { NextResponse } from "next/server";
-
 import { createClient } from "@/lib/supabase/server";
 
-export async function POST() {
-
-  const supabase =
-    await createClient();
+export async function POST(request: Request) {
+  const supabase = await createClient();
 
   await supabase.auth.signOut();
 
   return NextResponse.redirect(
-    new URL(
-      "/",
-      process.env.NEXT_PUBLIC_SITE_URL
-    )
+    new URL("/login", request.url)
   );
 }
